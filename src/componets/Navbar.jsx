e54ddebc11979bbafa0139c87f2e3a9893294ff2
Marlyn <marlinp230@gmail.com>
2023-03-03 09:14:42 -0500

    DEL

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+    const [Name, setName] = useState('')
+const OnSudmit= (e)=>{
+    e.preventDefault()
+
+}
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-2">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <Link className="navbar-brand" to="/">Home</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            </li>
+                            
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Link</Link>
                             </li>
@@ -24,9 +27,9 @@ function Navbar() {
                                 <Link className="nav-link disabled" to="/" tabindex="-1" aria-disabled="true">Disabled</Link>
                             </li>
                         </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" onSubmit={OnSudmit}>
+                            <input className="form-control me-2" type="text" placeholder="Add Name" aria-label="Search"name='Name'  value={Name} onChange={(e)=>setName(e.target.value)}/>
+                                <button className="btn btn-outline-dark" type="submit">Add</button>
                         </form>
                     </div>
                 </div>
--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -1,24 +1,36 @@
 import Axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Spiner from '../componets/Spiner'
 import moment from 'moment'
 import Swal from 'sweetalert2'
+import Navbar from '../componets/Navbar'
 
 
-const dates= ()=>{
-    return `${moment().format('L')}`
-  }
-  
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST': return { ...state, loading: true };
+        case 'FETCH_SUCCESS': return { ...state, Caja: action.payload, loading: false };
+        case 'FETCH_FAIL': return { ...state, loading: false, error: action.payload };
+        default: return state
+
+    }
+
+}
+
 function Client() {
-    const { Nombre ,id} = useParams() 
-    const [Client, setClient] = useState(null)
-    const [Caja, setCaja] = useState(null)
-    const totalCaja = Caja ? Caja.reduce((p, c) => p + c.Monto, 0) : 0
+    const [{ loading, error, Caja }, dispatch] = useReducer(reducer, { loading: true, error: '' })
 
-console.log(id)
-    // <------------------------------------------sweet alert add data to db------------------------------------------------------------------------>
-    const SwalAlert = async () => {
+    const { Nombre, id } = useParams()
+    const [Client, setClient] = useState(null)
+    // const [Caja, setCaja] = useState(null) 
+    const totalCaja = Caja ? Caja.reduce((p, c) => p + c.Monto, 0) : 0
+    const dates = () => {
+        return `${moment().format('L')}`
+    }
+
+    // <------------------------------------------ add data to db------------------------------------------------------------------------>
+    const FormAdd = async () => {
         Swal.fire({
             title: Nombre,
             html: `<input type="text" id="Nombre" class="swal2-input" value=${Nombre} disabled='true' placeholder="Nombre">
@@ -32,20 +44,20 @@ console.log(id)
                 const Detalle = Swal.getPopup().querySelector('#Detalle').value
                 const Monto = Swal.getPopup().querySelector('#Monto').value
                 const id2 = Swal.getPopup().querySelector('#id2').value
-                      console.log(id2,'L35')
-                if (!Detalle || !Monto || !Nombre || !id2 ) {
+                console.log(id2, 'L35')
+                if (!Detalle || !Monto || !Nombre || !id2) {
                     Swal.showValidationMessage(`Por favor ponga el Nombre Detalle and Monto.`)
                 }
-                return { Detalle: Detalle, Monto: Monto, Nombre,Fecha:dates(),id2 }
+                return { Detalle: Detalle, Monto: Monto, Nombre, Fecha: dates(), id2 }
             }
         }).then(async (result) => {
-               console.log(result.value.id2,'estre')
+            console.log(result.value.id2, 'estre')
             const res = await Axios.post('https://charming-dove-pantsuit.cyclic.app/v/', {
                 Nombre: result.value.Nombre,
                 Detalle: result.value.Detalle,
                 Monto: result.value.Monto,
-                Fecha:dates(),
-                id2:result.value.id2
+                Fecha: dates(),
+                id2: result.value.id2
 
             })
 
@@ -60,6 +72,45 @@ console.log(id)
 
         })
     }
+    // <-----------------------------------------------------------delete database caja------------------------------------------------------------------------>
+    const DeleteDATA = async (id) => {
+        Swal.fire({
+            title: Nombre,
+            html: `
+            <input type="text" id="id" class="swal2-input" value=${id} disabled='true' placeholder="ID">
+            <input type="text" id="id2" class="swal2-input"  placeholder="ID">
+       `,
+            confirmButtonText: 'Guardar',
+            focusConfirm: false,
+            preConfirm: () => {
+              
+                const id = Swal.getPopup().querySelector('#id').value
+                const id2 = Swal.getPopup().querySelector('#id2').value
+
+             
+                if (!id || !id2) {
+                    Swal.showValidationMessage(`Por favor ponga el id`)
+                }
+                return { id, id2 }
+            }
+        }).then(async (result) => {
+            console.log(result.value.id2,result.value.id, 'estre')
+        
+           if (result.value.id !== result.value.id2) {
+            Swal.fire({
+                title: `THE IDS ARE NOT THE SAME      ${result.value.id}-${result.value.id2}`
+            })
+           }
+
+
+        })
+       
+    }
+     // <-----------------------------------------------------------delete database caja------------------------------------------------------------------------>
+     const UpdateDATA = async (id) => {
+        console.log(id,'up')
+       
+    }
     // <-----------------------------------------------------------useEffect------------------------------------------------------------------------>
 
     useEffect(() => {
@@ -68,46 +119,68 @@ console.log(id)
 
     // <------------------------------------------getting data from data base------------------------------------------------------------------------>
     const data = async () => {
-        const res = await Axios.get(`https://charming-dove-pantsuit.cyclic.app/client/getclient/${Nombre}`);
-        setClient(res.data.client)
-        setCaja(res.data.cajadb)
-    }   
+        dispatch({ type: "FETCH_REQUEST" })
+        try {
+
+            const res = await Axios.get(`https://charming-dove-pantsuit.cyclic.app/client/getclient/${Nombre}`);
+
+           
+            setClient(res.data.client)
+
+            dispatch({ type: "FETCH_SUCCESS", payload: res.data.cajadb })
+            // setCaja(res.data.cajadb)
+        } catch (error) {
+            dispatch({ type: "FETCH_FAIL", payload: error.message })
+        }
+    }
     return (
         <>
-            <div className='container'>
-                <div className="row mt-4 ">
-                    <div className="">
-                        <button className='btn btn-primary' onClick={SwalAlert} >Add</button>
+        <Navbar/>
+            <div className='container mt-4 text-center'>
+                <div className="row  ">
+                    <div className="col-1 col-md-4 col-lg-4 card-body bg-info border d-flex">
+                       <button className='btn btn-dark' onClick={FormAdd} >Add</button>
+                        <p className=' p-2'>Nombre: {Client ? Client[0].Nombre : "None"}</p>
                     </div>
-                    <div className="col-sm-1 col-md-4 card-body bg-info border">
-                        <p className='m-2 p-2'><strong>Nombre: {Client ? Client[0].Nombre : "None"}</strong></p>
+                    <div className="col-1 col-md-4 col-lg-4 card-body bg-primary border">
+                        <p className=' p-2'>Fecha: {Client ? Client[0].Fecha : "No Date"} </p>
                     </div>
-
-                    <div className="col-sm-1 col-md-4 card-body bg-primary border">
-                        <p className='m-2 p-2'><strong>Fecha:{Client ? Client[0].Fecha : "No Date"} </strong></p>
+                    <div className={`col-1 col-md-4 col-lg-4 card-body border ${totalCaja > 0 ? "bg-primary" : " bg-danger"}`}>
+                        <p className={` p-2 `}>Total: ${totalCaja}</p>
                     </div>
-
-                    <div className={`col-sm-1 col-md-4 card-body border ${totalCaja > 0 ? "bg-primary" : " bg-danger"}`}>
-                        <p className={`m-2 p-2 `}><strong>Total: ${totalCaja}</strong></p>
-                    </div>
-
                 </div>
             </div>
-            <div className="container">
-                <div className="di"></div>
+            <div className="container ">
+                <div className="row text-center mb-2">
+                    <div className="col-1 col-md-4 col-lg-4 card-body bg-info border">
+                        <p className='m-2 p-2'>Nombre: {Client ? Client[0].Nombre : "None"}</p>
+                    </div>
+                    <div className="col-1 col-md-4 col-lg-4 card-body bg-primary border">
+                        <p className='m-2 p-2'>Fecha: {Client ? Client[0].Fecha : "No Date"} </p>
+                    </div>
+                    <div className={`col-1 col-md-4 col-lg-4 card-body border ${totalCaja > 0 ? "bg-primary" : " bg-danger"}`}>
+                        <p className={`m-2 p-2 `}>Total: ${totalCaja}</p>
+                       
+                    </div>
+                </div>
                 <div className="row">
                     {
                         Caja ? Caja.map((caja, index) => (
-                            <div className=" col-md-2 col-xl-3 card mb-2" key={index}>
+                            <div className=" col-md-4 col-xl-3 card mb-2" key={index}>
                                 <div className="card-body">
                                     <div className="nombre bg-info">Nombre: <strong>{caja.Nombre}</strong></div>
                                     <div className="Detalle">Detalle: {caja.Detalle}</div>
                                     <div className={`${caja.Monto > 0 ? 'bg-success' : "bg-danger"} `}>Monto: ${caja.Monto}</div>
                                     <div className="fecha bg-primary">Fecha: <strong> {caja.Fecha}</strong></div>
                                     <div className="id2">id2: {caja.id2}</div>
+                                    <div className="opts">
+                                        <button className='btn btn-info btn-sm m-2' onClick={()=>{UpdateDATA((caja._id))}}>Update</button>
+                                        <button className='btn btn-danger btn-sm m-2'onClick={()=>{DeleteDATA(caja._id)}}>Delete</button>
+
+                                    </div>
 
                                 </div>
-                            </div>)) : <Spiner />
+                            </div>)) : loading ? <Spiner /> : <h6>Error {error}</h6>
                     }
                 </div>
             </div>
